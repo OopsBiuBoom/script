dbc3de6232400641044edfc08721cf0ddf376142
Bii <>
2023-11-28 03:50:14 +0800

    多图片合成脚本

---
+++ autosyn/imgLoop.py
@@ -0,0 +1,36 @@
+# 用于单个语音和多张图片合成视频
+# 使用方法：把图片和mp3文件放在imgchange文件夹下即可。当前文件要在根目录下执行
+
+import os
+import glob
+import subprocess
+
+# 设置环境变量
+ffmpeg_PATH = r".\ffmpeg\bin"
+os.environ["PATH"] += os.pathsep + ffmpeg_PATH
+
+# 获取工作目录
+current_folder = os.getcwd()
+current_folder += "\imgchange"
+
+
+# 定义可操作的图片后缀
+image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.bmp']
+
+image_paths = []
+
+# 查找当前文件夹下所有图片
+for extension in image_extensions:
+    image_paths.extend(glob.glob(os.path.join(current_folder,extension)))
+    
+# 查找当前文件夹下MP3文件
+mp3Path = glob.glob(os.path.join(current_folder,"*.mp3"))[0]
+
+# py路径
+pyPath = ".\python38\python.exe"
+
+for index, imagePath in enumerate(image_paths):
+    com = f".\python38\python.exe .\inference.py --driven_audio {mp3Path} --source_image {imagePath} --result_dir {os.path.join(current_folder,str(index))} --preprocess resize --enhancer gfpgan"
+    subprocess.call(com)
+
+
